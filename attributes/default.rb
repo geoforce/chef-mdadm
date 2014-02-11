e375d2e33dd84933499f2e1bfa5380f3c63588bf
Kevin Berry <kevin@opensourcealchemist.com>
2014-02-11 10:31:39 -0600

    Resolves issue #1 - Adds initramfs-tools support on ubuntu to adjust BOOT_DEGRADED value.

---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+# Attribute controls whether the initramfs will boot through a degraded md raid or not.  We default to true (package default is false)
+default['mdadm']['boot_degraded'] = true
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,12 @@
 #
 
 package "mdadm"
+
+if node['platform'] == "ubuntu"
+  template "/etc/initramfs-tools/conf.d/mdadm" do
+    owner 'root'
+    group 'root'
+    mode '0644'
+    source 'initramfs-tools_conf.d_mdadm.erb'
+  end
+end
